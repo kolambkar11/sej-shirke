f95f2f6b1797d252ed7066d9927c7043fc637f42
kolambkar11 <root@VVI.local>
2023-07-19 12:48:41 +0530

    final changes done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Main from "./component/main/Main";
 import Notfound from "./component/notfound/Notfound";
 import Reels from "./component/reels/Reels";
 import Youtube from "./component/youtube/Youtube";
+import Vimeo from "./component/vimeo/Vimeo";
 function App() {
   return (
     <>
@@ -12,6 +14,7 @@ function App() {
           <Route activeClassName="active_class" exact path="/sej-shirke" element={<Main />}></Route>
           <Route activeClassName="active_class" exact path="/youtube" element={<Youtube />}></Route>
           <Route activeClassName="active_class" exact path="/reels" element={<Reels />}></Route>
+          <Route activeClassName="active_class" exact path="/vimeo" element={<Vimeo />}></Route>
           <Route path="*" element={<Notfound />}></Route>
         </Routes>
       </Router>
